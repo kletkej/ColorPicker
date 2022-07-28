cad3c7a7d608183fad191c99cbc7fa654e0ec7dd
kletkej <kletkej@gmail.com>
2022-07-28 21:38:53 +0200

    remove popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,7 @@
-function extractBtnClicked() {
-  //take screenshot
-  chrome.tabs.captureVisibleTab(null, {}, function (image) {
-    chrome.storage.local.set({"screenshot": image});
-    
-    //open new tab
-    chrome.tabs.create({ url: 'page/extract.html' });
-  });
-}
+//take screenshot
+chrome.tabs.captureVisibleTab(null, {}, function (image) {
+  chrome.storage.local.set({ "screenshot": image });
 
-//add/remove animated class on mouse event
-function particlesAnimation(e){
-  let divClassList = e.target.parentNode.querySelector(".particles").classList;
-  
-  if (divClassList.contains('animated')) {
-    divClassList.remove("animated");
-  } else {
-    divClassList.add("animated");
-  }
-}
-
-window.onload = () => {
-  var extractButton = document.getElementById("extractBtn");
-  extractButton.addEventListener("mouseenter", particlesAnimation);
-  extractButton.addEventListener("mouseleave", particlesAnimation);
-  extractButton.addEventListener("click", extractBtnClicked)
-}
+  //open new tab
+  chrome.tabs.create({ url: 'page/extract.html' });
+});
